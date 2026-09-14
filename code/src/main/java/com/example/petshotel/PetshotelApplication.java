f48eecc742457eb81457f8857b58abd1887a3800
Tanut <tanut23636@gmail.com>
2026-09-14 21:16:08 +0700

    chore: init project structure

---
+++ code/src/main/java/com/example/petshotel/PetshotelApplication.java
@@ -0,0 +1,13 @@
+package com.example.petshotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetshotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetshotelApplication.class, args);
+	}
+
+}
---
+++ code/src/test/java/com/example/petshotel/PetshotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petshotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
