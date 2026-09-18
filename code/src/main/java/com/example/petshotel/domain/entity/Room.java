f741db2b06f423ced9f51ecf65388fb429a7b14b
kittithat67 <boss25480za@gmail.com>
2026-09-18 21:04:27 +0700

    edit Room.java

--- code/src/main/java/com/example/petshotel/domain/entity/Room.java
+++ code/src/main/java/com/example/petshotel/domain/entity/Room.java
@@ -17,17 +17,22 @@ public class Room {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true)
+    @Column(unique = true,nullable = false)
     private String roomNumber;
     
+    @Column(nullable = false)
     private String name;
+
     private String description;
+
+    @Column(nullable = false)
     private String capacity;
 
     @Column(nullable = false,precision = 10,scale = 2)  //เลข 10  หลัก ทศนิยม 2 หลัก
     private BigDecimal pricePerPetPerNight;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private RoomStatus status;
 
 }
