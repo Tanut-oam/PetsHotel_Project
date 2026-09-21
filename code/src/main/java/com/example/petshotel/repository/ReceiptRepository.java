1998fabdc95ce12bc142fd17f60c3abc5badcbc7
Nadia <nordorja99@gmail.com>
2026-09-21 18:28:31 +0700

    add PromotionRepository, ExtraServiceRepository, BookingExtraServiceRepository and ReceiptRepository

---
+++ code/src/main/java/com/example/petshotel/repository/ReceiptRepository.java
@@ -0,0 +1,15 @@
+package com.example.petshotel.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.petshotel.domain.entity.Receipt;
+
+@Repository 
+public interface ReceiptRepository extends JpaRepository<Receipt, Long>{
+    Optional<Receipt> findByBookingId(Long bookingId);
+    Optional<Receipt> findByReceiptNumber(String receiptNumber);
+    
+}
