5d8ef1e39c86c598a3cfb7c92629c00a28ad0f76
Nadia <nordorja99@gmail.com>
2026-09-22 21:48:21 +0700

    refactor: remove unused imports in repositories

--- code/src/main/java/com/example/petshotel/repository/PromotionRepository.java
+++ code/src/main/java/com/example/petshotel/repository/PromotionRepository.java
@@ -3,7 +3,6 @@ package com.example.petshotel.repository;
 import com.example.petshotel.domain.entity.Promotion;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
  
 public interface PromotionRepository extends JpaRepository<Promotion, Long>{
--- code/src/main/java/com/example/petshotel/repository/ReceiptRepository.java
+++ code/src/main/java/com/example/petshotel/repository/ReceiptRepository.java
@@ -3,8 +3,6 @@ package com.example.petshotel.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.example.petshotel.domain.entity.Receipt;
 
 
