20c19e2fd3abe50ab83472cdb99aedf74d5ddb4a
kitithoughts-bit <kitithoughts@gmail.com>
2026-09-24 00:20:28 +0700

    Add create booking request DTO

--- code/src/main/java/com/example/petshotel/dto/request/CreateBookingRequest.java
+++ code/src/main/java/com/example/petshotel/dto/request/CreateBookingRequest.java
@@ -1,5 +1,24 @@
 package com.example.petshotel.dto.request;
 
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CreateBookingRequest {
-    
+
+    private Long userId;
+
+    private Long roomId;
+
+    private List<Long> petIds;
+
+    private LocalDate checkInDate;
+
+    private LocalDate checkOutDate;
 }
