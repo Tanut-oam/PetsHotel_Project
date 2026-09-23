50d44c3150527b241da2001adb172e90f399f070
Nadia <nordorja99@gmail.com>
2026-09-23 19:57:00 +0700

    feat: add findByCode method to PromotionRepository

--- code/src/main/java/com/example/petshotel/repository/PromotionRepository.java
+++ code/src/main/java/com/example/petshotel/repository/PromotionRepository.java
@@ -2,9 +2,12 @@ package com.example.petshotel.repository;
 
 import com.example.petshotel.domain.entity.Promotion;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
  
 public interface PromotionRepository extends JpaRepository<Promotion, Long>{
     List<Promotion> findByActiveTrue();
+    Optional<Promotion> findByCode(String code);
 }
